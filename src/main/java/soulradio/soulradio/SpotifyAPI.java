5a3b232038a5d9a0ba585c3396b3ae3dfcaa2e8d
alakijaayo <alakijaayo@gmail.com>
2020-06-21 18:04:43 +0100

    Initial Setup

---
+++ src/main/java/soulradio/soulradio/SpotifyAPI.java
@@ -0,0 +1,13 @@
+package soulradio.soulradio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyAPI {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotifyAPI.class, args);
+	}
+
+}
